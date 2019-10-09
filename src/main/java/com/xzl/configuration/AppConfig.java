e58853a5895cbaeaba31b016b428f0a843c7296d
Xianzhou.Liu <flynnhome.liu@gmail.com>
2019-10-09 21:00:17 +0800

    first commit.

---
+++ src/main/java/com/xzl/configuration/AppConfig.java
@@ -0,0 +1,11 @@
+package com.xzl.configuration;
+
+import com.xzl.test.CompactDisc;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    public CompactDisc compactDisc(){
+
+    }
+}
---
+++ src/main/java/com/xzl/test/CompactDisc.java
@@ -0,0 +1,7 @@
+package com.xzl.test;
+
+import org.springframework.stereotype.Component;
+
+public interface CompactDisc {
+    public void play();
+}
